063c560877ec7ea8e87d4362bc050aad7a614790
Richard <Richardenlavida1@gmail.com>
2026-09-12 15:37:16 -0500

    Prueba (Leer bits)

---
+++ build/Desktop_Qt_6_11_2_MinGW_64_bit_Debug/CMakeFiles/Desafio1-Informatica2.dir/bits.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/Desafio1-Informatica2.dir/bits.cpp.obj: \
+ C:\Users\user\OneDrive\Documentos\Desafio1-Informatica2\bits.cpp \
+ C:\Users\user\OneDrive\Documentos\Desafio1-Informatica2\bits.h
--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include "bits.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    unsigned char datos[2];
+
+    datos[0] = 0b01000000;
+    datos[1] = 0b00000000;
+
+    cout << obtenerFicha(datos, 6) << endl;
+
     return 0;
 }
