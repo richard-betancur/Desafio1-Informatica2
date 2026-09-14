e1e34a362c229f301c80b1e0c6febef8a959c0f2
Richard <Richardenlavida1@gmail.com>
2026-09-14 15:18:48 -0500

    Prueba Obtener/Poner Ficha

--- main.cpp
+++ main.cpp
@@ -5,12 +5,18 @@ using namespace std;
 
 int main()
 {
-    unsigned char datos[2];
+    unsigned char datos[3] = {0, 0, 0};
 
-    datos[0] = 0b01000000;
-    datos[1] = 0b00000000;
+    ponerFicha(datos, 0, 5);
+    ponerFicha(datos, 3, 2);
+    ponerFicha(datos, 6, 7);
+    ponerFicha(datos, 9, 4);
 
-    cout << obtenerFicha(datos, 6) << endl;
+    cout << "Ficha en bit 0: " << obtenerFicha(datos, 0) << endl;
+    cout << "Ficha en bit 3: " << obtenerFicha(datos, 3) << endl;
+    cout << "Ficha en bit 6: " << obtenerFicha(datos, 6) << endl;
+    cout << "Ficha en bit 9: " << obtenerFicha(datos, 9) << endl;
+
 
     return 0;
 }
